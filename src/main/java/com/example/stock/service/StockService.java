c24941b3455895b947949e5fe0ba2152ac72a5c1
heedoitdox <binzzang810@gmail.com>
2023-02-20 23:09:43 +0900

    feature: synchronized 사용하기

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -4,6 +4,8 @@ import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 public class StockService {
     private StockRepository stockRepository;
@@ -13,7 +15,8 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    public void decrease(Long id, Long quantity) {
+    @Transactional
+    public synchronized void decrease(Long id, Long quantity) {
         // get stock
         // 재고 감소
         // 저장
